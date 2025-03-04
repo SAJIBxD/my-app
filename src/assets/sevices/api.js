e134b15a84a9ac2aa2903f0b303c0adb7dc88324
sajibxd <88776229+HossainRizbi@users.noreply.github.com>
2025-03-04 23:10:51 +0600

    variable

--- src/assets/sevices/api.js
+++ src/assets/sevices/api.js
@@ -1,5 +1,5 @@
-const API_KEY = "40480dc0d6cf71c830fd0310844d9795"
-const BASE_URL = "https://api.themoviedb.org/3"
+const API_KEY = import.meta.env.VITE_API_KEY;
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const getMovies = async () => {
   const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
